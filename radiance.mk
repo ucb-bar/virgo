dc86d69da4dfc8dd3c5d852ed7b606df83ea736d
Hansung Kim <hansung_kim@berkeley.edu>
2024-01-29 15:40:15 -0800

    Add make fragment

---
+++ radiance.mk
@@ -0,0 +1,19 @@
+##############################################################
+# extra variables/targets ingested by the chipyard make system
+##############################################################
+
+##################################################################
+# THE FOLLOWING MUST BE += operators
+##################################################################
+
+RADPIE_SRC_DIR = $(base_dir)/generators/radiance/radpie
+RADPIE_BUILD_DIR = $(RADPIE_SRC_DIR)/target/release
+
+EXTRA_SIM_REQS += radpie
+EXTRA_SIM_LDFLAGS += -L$(RADPIE_BUILD_DIR) -Wl,-rpath,$(RADPIE_BUILD_DIR) -lradpie
+
+# cargo handles building of Rust files all on its own, so make this a PHONY
+# target to run cargo unconditionally
+.PHONY: radpie
+radpie:
+	cd $(RADPIE_SRC_DIR) && cargo build --release
