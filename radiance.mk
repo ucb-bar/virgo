064f4f854110311a34bc39a2727cced2eb6ef272
Hansung Kim <hansung_kim@berkeley.edu>
2024-02-01 23:56:38 -0800

    Move EXTRA_SIM_PREPROC_DEFINES to mk fragment

--- radiance.mk
+++ radiance.mk
@@ -11,6 +11,11 @@ RADPIE_BUILD_DIR = $(RADPIE_SRC_DIR)/target/release
 
 EXTRA_SIM_REQS += radpie
 EXTRA_SIM_LDFLAGS += -L$(RADPIE_BUILD_DIR) -Wl,-rpath,$(RADPIE_BUILD_DIR) -lradpie
+EXTRA_SIM_PREPROC_DEFINES += \
+	+define+SIMULATION \
+	+define+SV_DPI \
+	+define+GPR_RESET \
+	+define+DBG_TRACE_CORE_PIPELINE_VCS
 
 # cargo handles building of Rust files all on its own, so make this a PHONY
 # target to run cargo unconditionally
